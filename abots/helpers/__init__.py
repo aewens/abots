15eddbcb82a12892835ec9d92b12872cb5daab65
aewens <aewens@tilde.center>
2019-05-06 22:19:34 +0200

    Added logging system

--- abots/helpers/__init__.py
+++ abots/helpers/__init__.py
@@ -2,3 +2,4 @@ from abots.helpers.hash import create_hash, md5, sha1, sha256, sha512
 from abots.helpers.encode import jots, jsto, b64e, b64d, h2b64, b642h, ctos
 from abots.helpers.numbers import clamp, randfloat
 from abots.helpers.general import eprint, deduce, noop, cast, get_digit
+from abots.helpers.logging import Logger
